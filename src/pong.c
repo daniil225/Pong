d511b78f7da763dc5ec6eb8c8bc03065bbfcf2c7
Manfryd Erlene <manfryde@cl-c2.nsk.21-school.ru>
2022-07-03 15:33:59 +0700

    Release 0.2

--- src/pong.c
+++ src/pong.c
@@ -3,13 +3,15 @@
 #define WIDTH_SCREEN 82
 #define HEIGHT_SCREEN 27
 
-/* Настройки позиции рокетоок и мяча */
+/* Настройки позиции рокетоок и мяча и его направления движения */
 #define START_POSITION_P1_X 2
 #define START_POSITION_P1_Y 14
 #define START_POSITION_P2_X 78
 #define START_POSITION_P2_Y 14
 #define START_POSITION_BALL_X 40
 #define START_POSITION_BALL_Y 14
+#define START_DIRECTION_BALL_X 1
+#define START_DIRECTION_BALL_Y 1
 #define START_SCORE_P 0
 /*  Информационное табло */
 #define START_POSITION_INFORMATION 0
@@ -24,6 +26,7 @@
 #define Z 122
 #define K 107
 #define M 109
+#define ENTER 10
 
 
 
@@ -54,6 +57,9 @@ typedef struct Ball
 // Вызывается при первой отрисовке тоесть когда значения всех переменных игры соответствуют default param
 void Drow_Screen(P1 *p1, P2 *p2, Ball *ball);
 void Move_Rocket(char ch, P1 *p1, P2 *p2, Ball *ball);
+void Move_Ball(char ch,P1 *p1, P2 *p2, Ball *ball);
+
+void ClearScreen();
 
 int main() {
     
@@ -69,11 +75,21 @@ int main() {
     p2.score = START_SCORE_P;
     ball.x = START_POSITION_BALL_X;
     ball.y = START_POSITION_BALL_Y;
-    ball.directionX = 1;
-    ball.directionY = 1;
-    Drow_Screen(&p1, &p2, &ball);
-    char key = getchar();
-    Move_Rocket(key, &p1, &p2, &ball);
+    ball.directionX = START_DIRECTION_BALL_X;
+    ball.directionY = START_DIRECTION_BALL_Y;
+    
+    
+    
+    
+    while(1)
+    {
+        Drow_Screen(&p1, &p2, &ball);
+        char key = getchar();
+        Move_Rocket(key, &p1, &p2, &ball);
+        Move_Ball(key, &p1, &p2, &ball);
+        ClearScreen();
+    }
+    
     return 0;
 }
 
@@ -108,27 +124,65 @@ void Move_Rocket(char ch, P1 *p1, P2 *p2, Ball *ball)
     // Обработка движения ракетки первого игрока
     if(ch == A)
     {
-        if((p1->y - 1) == 3) Drow_Screen(p1, p2, ball);
+        if((p1->y - 1) == 4)
+        {
+            p1->y = p1->y;
+        }
         else
         {
-            p1->y++;
-            Drow_Screen(p1, p2, ball);
+            p1->y--;
         }
     }
     if(ch == Z)
     {
-        p1->y--;
-        Drow_Screen(p1, p2, ball);
+        if(p1->y + 1 == HEIGHT_SCREEN - 2)
+        {
+            p1->y = p1->y;
+        }
+        else
+        {
+        p1->y++;
+        }
     }
     // Обработка движения ракетки второго игрока
-    if(ch == K)    {
-        p2->y++;
-        Drow_Screen(p1, p2, ball);
+    if(ch == K)
+    {
+        if(p2->y -1 == 4)
+        {
+            p2->y == p2->y;
+        }
+        else
+        {
+            p2->y--;
+        }
     }
     if(ch == M)
     {
-        p2->y--;
-        Drow_Screen(p1, p2, ball);
+        if(p2->y + 1 == HEIGHT_SCREEN - 2)
+        {
+            p2->y = p2->y;
+        }
+        else
+        {
+            p2->y++;
+        }
     }
- 
+}
+
+void Move_Ball(char ch,P1 *p1, P2 *p2, Ball *ball)
+{
+    if(ch == ENTER)
+    {
+        ball->x += ball->directionX;
+        ball->y += ball->directionY;
+    }
+    
+    
+}
+
+
+
+void ClearScreen()
+{
+    printf("\33[0d\33[2J");
 }
