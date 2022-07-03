c9e574bf2dbf32671f1fc7446c61e759ce02288c
Manfryd Erlene <manfryde@cl-c2.nsk.21-school.ru>
2022-07-03 17:46:42 +0700

    Release 1.0

--- src/pong.c
+++ src/pong.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
+// Логические константы ////////////////////////////////////
+
+#define TRUE 1
+#define FALSE 0
+
+
 #define WIDTH_SCREEN 82
 #define HEIGHT_SCREEN 27
 
@@ -10,9 +16,11 @@
 #define START_POSITION_P2_Y 14
 #define START_POSITION_BALL_X 40
 #define START_POSITION_BALL_Y 14
-#define START_DIRECTION_BALL_X 1
+#define START_DIRECTION_BALL_X -1
 #define START_DIRECTION_BALL_Y 1
 #define START_SCORE_P 0
+#define SCORE_TO_WIN 21 // Потом поменяем на 21
+
 /*  Информационное табло */
 #define START_POSITION_INFORMATION 0
 #define END_POSITION_INFORMATION 2
@@ -26,23 +34,23 @@
 #define Z 122
 #define K 107
 #define M 109
+#define Q 113
+#define R 114
 #define ENTER 10
 
 
+// Игровые объекты  ///////////////////////////////////////////////////////////
 
-typedef struct P1
+// Структура игрокв //////////////////////////////////////////////////////////
+typedef struct Player
 {
     int x;
     int y;
     int score;
-} P1;
+} Player;
 
-typedef struct P2
-{
-    int x;
-    int y;
-    int score;
-}P2;
+
+// Структура Мяча ///////////////////////////////////////////////////////////
 
 typedef struct Ball
 {
@@ -54,30 +62,34 @@ typedef struct Ball
     
 } Ball;
 
-// Вызывается при первой отрисовке тоесть когда значения всех переменных игры соответствуют default param
-void Drow_Screen(P1 *p1, P2 *p2, Ball *ball);
-void Move_Rocket(char ch, P1 *p1, P2 *p2, Ball *ball);
-void Move_Ball(char ch,P1 *p1, P2 *p2, Ball *ball);
+// ///////////////////////////////////////////////////////////////////////////
 
+// Управляет игровым процессом
+void Play(char key,Player *p1, Player *p2, Ball *ball);
+void EndGame(char key,Player *p1, Player *p2, Ball *ball);
+
+// ///////////////////////////////////////////////////////////////////////////
+
+// Функции управления игровыми объектами
+void Init_Objects(Player *p1, Player *p2, Ball *ball);
+void Drow_Screen(Player *p1, Player *p2, Ball *ball);
+void Move_Rocket(char ch, Player *p1, Player *p2, Ball *ball);
+void Move_Ball(Player *p1, Player *p2, Ball *ball);
+
+//  ////////////////////////////////////////////////////////////////////////
+
+// Служебные функции
+
+// Функция очистки экрана
 void ClearScreen();
-
+//  ////////////////////////////////////////////////////////////////////////
 int main() {
     
-    P1 p1;
-    P2 p2;
+    Player p1;
+    Player p2;
     Ball ball;
     /* Инициализируем положения рокеток и положение мяча  */
-    p1.x = START_POSITION_P1_X;
-    p1.y = START_POSITION_P2_Y;
-    p1.score = START_SCORE_P;
-    p2.x = START_POSITION_P2_X;
-    p2.y = START_POSITION_P2_Y;
-    p2.score = START_SCORE_P;
-    ball.x = START_POSITION_BALL_X;
-    ball.y = START_POSITION_BALL_Y;
-    ball.directionX = START_DIRECTION_BALL_X;
-    ball.directionY = START_DIRECTION_BALL_Y;
-    
+    Init_Objects(&p1, &p2, &ball);
     
     
     
@@ -85,16 +97,38 @@ int main() {
     {
         Drow_Screen(&p1, &p2, &ball);
         char key = getchar();
-        Move_Rocket(key, &p1, &p2, &ball);
-        Move_Ball(key, &p1, &p2, &ball);
-        ClearScreen();
+        if(key != Q)
+        {
+            if((p1.score != SCORE_TO_WIN) && (p2.score != SCORE_TO_WIN)) Play(key, &p1, &p2, &ball);
+            else EndGame(key, &p1, &p2, &ball);
+            ClearScreen();
+        }
+        else
+        {
+            break;
+        }
     }
     
     return 0;
 }
 
 
-void Drow_Screen(P1 *p1, P2 *p2, Ball *ball)
+void Init_Objects(Player *p1, Player *p2, Ball *ball)
+{
+    /* Инициализируем положения рокеток и положение мяча  */
+    p1->x = START_POSITION_P1_X;
+    p1->y = START_POSITION_P2_Y;
+    p1->score = START_SCORE_P;
+    p2->x = START_POSITION_P2_X;
+    p2->y = START_POSITION_P2_Y;
+    p2->score = START_SCORE_P;
+    ball->x = START_POSITION_BALL_X;
+    ball->y = START_POSITION_BALL_Y;
+    ball->directionX = START_DIRECTION_BALL_X;
+    ball->directionY = START_DIRECTION_BALL_Y;
+}
+
+void Drow_Screen(Player *p1, Player *p2, Ball *ball)
 {
     for(int i = 0; i < HEIGHT_SCREEN; i++)
     {
@@ -105,8 +139,24 @@ void Drow_Screen(P1 *p1, P2 *p2, Ball *ball)
             else if((i == 3)&& (j >= 1)) printf("-");
             else if(i == ball->y && j == ball->x) printf("o");
             else if((i == END_POSITION_INFORMATION) && (j >= 1)) printf("-");
-            else if(i == 1 && j == START_INPUT_TO_EXIT) printf("Please enter ctrl + c to exit");
-            else if(i == 1 && j == START_PLAYER_SCORE) printf("player1: %d  |  player2: %d        |", p1->score, p2->score);
+            else if(i == 1 && j == START_INPUT_TO_EXIT)
+            {
+                if(p1->score == SCORE_TO_WIN || p2->score == SCORE_TO_WIN)
+                {
+                    printf("Please enter r to reset q exit");
+                }
+                else
+                {
+                    printf("Please enter q to exit        ");
+                }
+            }
+           
+            else if(i == 1 && j == START_PLAYER_SCORE)
+            {
+                if(p1->score == SCORE_TO_WIN) printf("player1: WIN  |  player2: LOSS  |");
+                else if(p2->score == SCORE_TO_WIN) printf("player1: LOSS |  player2: WIN  |");
+                else printf("player1: %d  |  player2: %d      |", p1->score, p2->score);
+            }
             else if(i != 1 && i != (HEIGHT_SCREEN-1) && j == (WIDTH_SCREEN-1)/2) printf("|");
             else if(((j == p1->x) && (i == p1->y)) || ((j == p1->x) && (i == p1->y -1)) || ((j == p1->x) && (i == p1->y + 1))) printf("|");
             else if((i == HEIGHT_SCREEN - 1) && (j >= 1)) printf("-");// Печатаем нижную границу доски
@@ -119,7 +169,7 @@ void Drow_Screen(P1 *p1, P2 *p2, Ball *ball)
    
 }
 
-void Move_Rocket(char ch, P1 *p1, P2 *p2, Ball *ball)
+void Move_Rocket(char ch, Player *p1, Player *p2, Ball *ball)
 {
     // Обработка движения ракетки первого игрока
     if(ch == A)
@@ -169,9 +219,55 @@ void Move_Rocket(char ch, P1 *p1, P2 *p2, Ball *ball)
     }
 }
 
-void Move_Ball(char ch,P1 *p1, P2 *p2, Ball *ball)
+void Move_Ball(Player *p1, Player *p2, Ball *ball)
 {
-    if(ch == ENTER)
+    if(ball->y == HEIGHT_SCREEN - 2)
+    {
+        ball->directionY = (-1)*ball->directionY;
+        ball->x += ball->directionX;
+        ball->y += ball->directionY;
+    }
+    else if(ball->y == 4)
+    {
+        ball->directionY = (-1)*ball->directionY;
+        ball->x += ball->directionX;
+        ball->y += ball->directionY;
+    }
+    else if((ball->x == p2->x-1) && (ball->y == p2->y-1 || ball->y == p2->y || ball->y == p2->y+1))
+    {
+        ball->directionX = (-1)*ball->directionX;
+        ball->x += ball->directionX;
+        ball->y += ball->directionY;
+    }
+    else if((ball->x == p1->x+1) && (ball->y == p1->y-1 || ball->y == p1->y || ball->y == p1->y+1))
+    {
+        ball->directionX = (-1)*ball->directionX;
+        ball->x += ball->directionX;
+        ball->y += ball->directionY;
+    }
+    else if(ball->x == 0)
+    {
+        p2->score++;
+        // Направление движения мяча выставлено в сторону проигравшего
+        ball->directionX = -1;
+        ball->directionY = 1;
+        // Устанавливаем позицию в начало
+        ball->x = START_POSITION_BALL_X;
+        ball->y = START_POSITION_BALL_Y;
+    }
+    else if(ball->x == WIDTH_SCREEN - 1)
+    {
+        p1->score++;
+            
+        // Направление движения мяча выставлено в сторону проигравшего
+        ball->directionX = 1;
+        ball->directionY = 1;
+            
+        // Устанавливаем позицию в начало
+        ball->x = START_POSITION_BALL_X;
+        ball->y = START_POSITION_BALL_Y;
+    }
+    else
     {
         ball->x += ball->directionX;
         ball->y += ball->directionY;
@@ -181,6 +277,22 @@ void Move_Ball(char ch,P1 *p1, P2 *p2, Ball *ball)
 }
 
 
+void Play(char key, Player *p1, Player *p2, Ball *ball)
+{
+    if(key == ENTER) Move_Ball(p1, p2, ball);
+    else if(key == A || key == Z || key == K || key == M)
+    {
+        Move_Rocket(key,p1, p2, ball);
+        Move_Ball(p1, p2, ball);
+    }
+}
+
+
+void EndGame(char key,Player *p1, Player *p2, Ball *ball)
+{
+    Drow_Screen(p1, p2, ball);
+    if(key == R) Init_Objects(p1, p2, ball);
+}
 
 void ClearScreen()
 {
